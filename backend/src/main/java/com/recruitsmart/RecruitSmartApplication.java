bd981e960dd5a0901ba0f2410e44e38ebe60dcc0
RecruitSmart <recruitsmart@example.com>
2026-01-23 15:46:58 +0530

    Super-Nuclear Memory Optimization (512MB Limit)

--- backend/src/main/java/com/recruitsmart/RecruitSmartApplication.java
+++ backend/src/main/java/com/recruitsmart/RecruitSmartApplication.java
@@ -12,7 +12,20 @@ import org.springframework.scheduling.annotation.EnableScheduling;
     org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration.class,
     org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration.class,
     org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration.class
+    org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.transaction.TransactionHealthContributorAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.couchbase.CouchbaseDataAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.ldap.LdapDataAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.webservices.WebServicesAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.mail.MailHealthContributorAutoConfiguration.class
 })
 @EnableScheduling
 public class RecruitSmartApplication {
